f1a0ec4460c06bd0fc664bfd5f6f12979cd89be8
da-imran <ios.imrannn@gmail.com>
2025-04-22 22:08:06 +0800

    Add 3 new modules

--- modules/shops.js
+++ modules/shops.js
@@ -1,5 +1,250 @@
 const mongo = require('../utilities/mongodb');
 
 module.exports = (app, config) => {
-	// your module logic here
+	const { mongoClient } = config;
+	const ROUTE_PREPEND = process.env.ROUTE_PREPEND;
+	const VERSION = process.env.VERSION;
+
+	// Get All Shops API
+	app.get(`/${ROUTE_PREPEND}/${VERSION}/shops`, async (req, res) => {
+		const apiName = 'Get All Shops API';
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			const shopResult = await mongo.find(mongoClient, 'shops');
+
+			if (shopResult) {
+				console.log(`${apiName} Response Success.`);
+				res.status(200).send({
+					status: 200,
+					data: shopResult
+				});
+			} else {
+				console.log(`❌ ${apiName} Response Failed.`);
+				res.status(404).send({
+					status: 404,
+					message: 'Shops not found',
+				});
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
+
+	// Create Shops API
+	app.post(`/${ROUTE_PREPEND}/${VERSION}/shops`, async (req, res) => {
+		const apiName = 'Create Shops API';
+		const {
+			name,
+			category,
+			status,
+			owner,
+			contact,
+			images,
+			location,
+			openingHours,
+		} = req.body;
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			if (!name) {
+				console.log(`❌ ${apiName} Bad request: name is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: name is a required parameter.',
+				});
+			} else if (!category) {
+				console.log(`❌ ${apiName} Bad request: category is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: category is a required parameter.',
+				});
+			} else if (!status) {
+				console.log(`❌ ${apiName} Bad request: status is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: status is a required parameter.',
+				});
+			} else if (!owner) {
+				console.log(`❌ ${apiName} Bad request: owner is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: owner is a required parameter.',
+				});
+			} else if (!contact) {
+				console.log(`❌ ${apiName} Bad request: contact is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: contact is a required parameter.',
+				});
+			} else if (!images) {
+				console.log(`❌ ${apiName} Bad request: images is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: images is a required parameter.',
+				});
+			} else if (!location) {
+				console.log(`❌ ${apiName} Bad request: location is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: location is a required parameter.',
+				});
+			} else if (!openingHours) {
+				console.log(`❌ ${apiName} Bad request: openingHours is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: openingHours is a required parameter.',
+				});
+			} else {
+				// 🔎 Proceed to create shop
+				const inputShop = {
+					name,
+					category,
+					status,
+					owner,
+					contact,
+					images,
+					location,
+					openingHours,
+					createdAt: new Date(),
+				};
+				const inputResult = await mongo.insertOne(mongoClient, 'shops', inputShop);
+				if (inputResult) {
+					console.log(`${apiName} MongoDB Success.`);
+					return res.status(200).json({
+						message: 'Shop created successfully',
+						adminId: inputResult.insertedId,
+					});
+				} else {
+					console.error('❌ Error creating shop.');
+					res.status(404).send({
+						status: 404,
+						message: 'Error creating shop.',
+					});
+				}
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
+
+	// Update Shops API by shopId
+	app.patch(`/${ROUTE_PREPEND}/${VERSION}/shops/:shopId`, async (req, res) => {
+		const { shopId } = req.params;
+		const {
+			name,
+			category,
+			status,
+			owner,
+			contact,
+			images,
+			location,
+			openingHours,
+		} = req.body;
+
+		const apiName = 'Update Shops API';
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			if (!shopId) {
+				console.log(`❌ ${apiName} Bad request: shopId is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: shopId is a required parameter.',
+				});
+			} else {
+				const updateObj = {};
+
+				if (name) updateObj.name = name;
+				if (category) updateObj.category = category;
+				if (status) updateObj.stack = status;
+				if (owner) updateObj.owner = owner;
+				if (contact) updateObj.contact = contact;
+				if (images) updateObj.images = images;
+				if (location) updateObj.location = location;
+				if (openingHours) updateObj.openingHours = openingHours;
+				updateObj.updatedAt = new Date();
+
+				const updateResult = await mongo.findOneAndUpdate(mongoClient, 'shops', { _id: mongo.getObjectId(shopId) }, updateObj);
+				if (!updateResult) {
+					res.status(404).send({
+						status: 404,
+						message: 'Shop not updated'
+					});
+				} else {
+					res.status(200).send({
+						status: 200,
+						message: 'Shop updated successfully.',
+						data: JSON.parse(JSON.stringify(updateResult)),
+					});
+				}
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
+
+	// Delete Shops API by shopId
+	app.delete(`/${ROUTE_PREPEND}/${VERSION}/shops/:shopId`, async (req, res) => {
+		const { shopId } = req.params;
+
+		const apiName = 'Delete Shops API';
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			if (!shopId) {
+				res.status(400).send({
+					status: 400,
+					message: 'Bad Request: shopId is a required parameters.',
+				});
+			} else {
+				const deleteResult = await mongo.deleteOne(mongoClient, 'shops', { _id: mongo.getObjectId(shopId) });
+				if (deleteResult) {
+					res.status(200).send({
+						status: 200,
+						message: 'Shop deleted successfully.',
+						data: {
+							adminUser: deleteResult
+						},
+					});
+				} else {
+					res.status(404).send({
+						status: 404,
+						message: 'Shop not deleted'
+					});
+				}
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
 };
--- modules/updates.js
+++ modules/updates.js
@@ -1,5 +1,224 @@
 const mongo = require('../utilities/mongodb');
 
 module.exports = (app, config) => {
-	// your module logic here
+	const { mongoClient } = config;
+	const ROUTE_PREPEND = process.env.ROUTE_PREPEND;
+	const VERSION = process.env.VERSION;
+
+	// Get All News API
+	app.get(`/${ROUTE_PREPEND}/${VERSION}/news`, async (req, res) => {
+		const apiName = 'Get All News API';
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			const newsResult = await mongo.find(mongoClient, 'news');
+
+			if (newsResult) {
+				console.log(`${apiName} Response Success.`);
+				res.status(200).send({
+					status: 200,
+					data: newsResult
+				});
+			} else {
+				console.log(`❌ ${apiName} Response Failed.`);
+				res.status(404).send({
+					status: 404,
+					message: 'News not found',
+				});
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
+
+	// Create News API
+	app.post(`/${ROUTE_PREPEND}/${VERSION}/news`, async (req, res) => {
+		const apiName = 'Create News API';
+		const {
+			title,
+			message,
+			category,
+			visibility,
+			adminId,
+		} = req.body;
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			if (!title) {
+				console.log(`❌ ${apiName} Bad request: title is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: title is a required parameter.',
+				});
+			} else if (!category) {
+				console.log(`❌ ${apiName} Bad request: category is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: category is a required parameter.',
+				});
+			} else if (!message) {
+				console.log(`❌ ${apiName} Bad request: message is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: message is a required parameter.',
+				});
+			} else if (!visibility) {
+				console.log(`❌ ${apiName} Bad request: visibility is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: visibility is a required parameter.',
+				});
+			} else if (!adminId) {
+				console.log(`❌ ${apiName} Bad request: adminId is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: adminId is a required parameter.',
+				});
+			} else {
+				// 🔎 Proceed to create shop
+				const inputNews = {
+					title,
+					message,
+					category,
+					visibility,
+					adminId,
+					createdAt: new Date(),
+				};
+				const inputResult = await mongo.insertOne(mongoClient, 'news', inputNews);
+				if (inputResult) {
+					console.log(`${apiName} MongoDB Success.`);
+					return res.status(200).json({
+						message: 'News created successfully',
+						adminId: inputResult.insertedId,
+					});
+				} else {
+					console.error('❌ Error creating news.');
+					res.status(404).send({
+						status: 404,
+						message: 'Error creating news.',
+					});
+				}
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
+
+	// Update News API by shopId
+	app.patch(`/${ROUTE_PREPEND}/${VERSION}/news/:newsId`, async (req, res) => {
+		const { newsId } = req.params;
+		const {
+			title,
+			message,
+			category,
+			visibility,
+			adminId,
+		} = req.body;
+
+		const apiName = 'Update News API';
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			if (!newsId) {
+				console.log(`❌ ${apiName} Bad request: newsId is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: newsId is a required parameter.',
+				});
+			} else if (!adminId) {
+				console.log(`❌ ${apiName} Bad request: adminId is a required parameter.`);
+
+				res.status(400).send({
+					status: 400,
+					message: 'Bad request: adminId is a required parameter.',
+				});
+			} else {
+				const updateObj = {};
+
+				if (title) updateObj.title = title;
+				if (category) updateObj.category = category;
+				if (message) updateObj.message = message;
+				if (visibility) updateObj.visibility = visibility;
+				if (adminId) updateObj.adminId = adminId;
+				updateObj.updatedAt = new Date();
+
+				const updateResult = await mongo.findOneAndUpdate(mongoClient, 'news', { _id: mongo.getObjectId(newsId) }, updateObj);
+				if (!updateResult) {
+					res.status(404).send({
+						status: 404,
+						message: 'News not updated'
+					});
+				} else {
+					res.status(200).send({
+						status: 200,
+						message: 'News updated successfully.',
+						data: JSON.parse(JSON.stringify(updateResult)),
+					});
+				}
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
+
+	// Delete News API by shopId
+	app.delete(`/${ROUTE_PREPEND}/${VERSION}/news/:newsId`, async (req, res) => {
+		const { newsId } = req.params;
+
+		const apiName = 'Delete News API';
+		try {
+			console.log(`${apiName} is called at ${new Date()}}`);
+
+			if (!newsId) {
+				res.status(400).send({
+					status: 400,
+					message: 'Bad Request: newsId is a required parameters.',
+				});
+			} else {
+				const deleteResult = await mongo.deleteOne(mongoClient, 'news', { _id: mongo.getObjectId(newsId) });
+				if (deleteResult) {
+					res.status(200).send({
+						status: 200,
+						message: 'News deleted successfully.',
+						data: {
+							adminUser: deleteResult
+						},
+					});
+				} else {
+					res.status(404).send({
+						status: 404,
+						message: 'News not deleted'
+					});
+				}
+			}
+		} catch (err) {
+			const error = { message: err.message, stack: err.stack };
+			res.status(500).send({
+				status: 500,
+				message: `${apiName} error`,
+				error,
+			});
+		}
+	});
 };
