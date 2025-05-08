e29dc3a25f6011e98f94af6d5ef80d1fa2ee1fb9
da-imran <ios.imrannn@gmail.com>
2025-05-08 21:29:25 +0800

    Fix require module of updates to news

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = async (app, config) => {
 	require('./modules/adminUsers')(app, config);
 	require('./modules/reports')(app, config);
 	require('./modules/residents')(app, config);
-	require('./modules/updates')(app, config);
+	require('./modules/news')(app, config);
 	require('./modules/events')(app, config);
 	require('./modules/shops')(app, config);
 	require('./modules/parks')(app, config);
