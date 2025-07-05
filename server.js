01d1ada9906cde96d127b50116a6951fb355addb
Muhammad Ehsan Imran <ios.imrannn@gmail.com>
2025-07-05 16:59:48 +0800

    Add port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const { app } = require('./app');
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
 	console.log(`🚀 JiranTetangga Backend listening at http://localhost:${PORT}`);
 });
