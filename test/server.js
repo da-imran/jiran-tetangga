a0784197f424008a1d6d798da2d77c9567fbf38c
Muhammad Ehsan Imran <ios.imrannn@gmail.com>
2025-07-20 16:57:53 +0800

    Removed authentication

--- test/server.js
+++ test/server.js
@@ -1,13 +1,10 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
-const authentication = require('../middleware/authentication');
 const mongodb = require('../utilities/mongodb');
 dotenv.config();
 
 const app = express();
-
-const environment = process.env.RUN_ENV || 'test';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '50mb' }));
@@ -17,15 +14,6 @@ app.use(cors({
 	methods: 'PATCH, POST, GET, DELETE, OPTIONS, PUT',
 	allowedHeaders: 'Origin, X-Requested-With, Content-disposition, Content-Type, Accept, Authorization, x-api-key'
 }));
-
-app.use((req, res, next) => {
-	if (environment === 'local') return next();
-
-	const excludedPaths = ['/', '/jiran-tetangga/v1'];
-	if (excludedPaths.includes(req.path)) return next();
-
-	return authentication(req, res, next);
-});
 
 module.exports = async () => {
 	const mongoClient = await mongodb.clientConnect(process.env.MONGO_URI);
