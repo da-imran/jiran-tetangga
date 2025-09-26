c2440e41ae2120c241532f01e1309f00c048d4dc
Muhammad Ehsan Imran <53717560+da-imran@users.noreply.github.com>
2025-09-26 10:44:07 +0800

    Update shops.js

--- modules/shops.js
+++ modules/shops.js
@@ -124,8 +124,8 @@ module.exports = (app, config) => {
 					});
 				} else {
 					console.log(`❌ ${apiName} Response Failed.`);
-					res.status(500).send({
-						status: 500,
+					res.status(404).send({
+						status: 404,
 						message: 'Shops not found',
 					});
 
@@ -133,7 +133,7 @@ module.exports = (app, config) => {
 						service: SERVICE_NAME,
 						module: MODULE,
 						apiName,
-						status: 500,
+						status: 404,
 						message: 'Shops not found',
 						data: shopResult,
 						traceId,
@@ -211,8 +211,8 @@ module.exports = (app, config) => {
 					});
 				} else {
 					console.log(`❌ ${apiName} Response Failed.`);
-					res.status(500).send({
-						status: 500,
+					res.status(404).send({
+						status: 404,
 						message: 'Shop not found',
 					});
 
@@ -220,7 +220,7 @@ module.exports = (app, config) => {
 						service: SERVICE_NAME,
 						module: MODULE,
 						apiName,
-						status: 500,
+						status: 404,
 						message: 'Shops not found',
 						data: shopResult,
 						traceId,
